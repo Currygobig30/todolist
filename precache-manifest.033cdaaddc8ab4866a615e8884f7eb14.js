c01d7a96b80ea4f231460ee7a916c3eb27973ff9
Chengyan <pcy00086@gmail.com>
2020-07-09 19:47:48 -0500

    Updates

---
+++ precache-manifest.033cdaaddc8ab4866a615e8884f7eb14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad219421efda3b121d7fc1fbb791a64",
+    "url": "/Currygobig30/todolist.git/index.html"
+  },
+  {
+    "revision": "91352a834b9c480f0860",
+    "url": "/Currygobig30/todolist.git/static/css/main.b260f974.chunk.css"
+  },
+  {
+    "revision": "7b4155da2a6a85110559",
+    "url": "/Currygobig30/todolist.git/static/js/2.cf37e1e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currygobig30/todolist.git/static/js/2.cf37e1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91352a834b9c480f0860",
+    "url": "/Currygobig30/todolist.git/static/js/main.792263e3.chunk.js"
+  },
+  {
+    "revision": "42998bef923d7b4f7b53",
+    "url": "/Currygobig30/todolist.git/static/js/runtime-main.b01e17bd.js"
+  }
+]);
